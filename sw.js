90f54812f6c7d189f04abe9077b080d1a63f50dd
TMN320 <a.tumenas@hotmail.com>
2026-03-01 11:45:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,5 @@
-const CACHE="atr-quiz-v1";
-const ASSETS=[
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/maskable-512.png"
-];
-self.addEventListener("install",(e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+
+self.addEventListener("install",e=>{
+ self.skipWaiting();
 });
-self.addEventListener("activate",(e)=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener("fetch",(e)=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener("fetch",e=>{});
